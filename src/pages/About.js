ccd1fc81b86aec4937f5e5a9a9780e005e34acd1
JMn06 <144167105+JMn06@users.noreply.github.com>
2023-10-25 04:55:37 +0000

    Added Sprint #4

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import './About.css'
+import PurchasesImg from '../assets/purchases.png';
 
 const About = () => {
     return (
-      <p>?</p>  
+      <><div className="centered-text">
+        <p>Somos una pequeña empresa de inmobilarias,
+          la cúal se dedica a enviar los productos a través de internert</p>
+      </div><div className="image-container">
+          <img src={PurchasesImg} alt="Purchases" />
+        </div></>
     );
 };
 
