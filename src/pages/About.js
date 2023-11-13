e15614fce54d0e464c5f067aa84b902e9827f787
JMn06 <144167105+JMn06@users.noreply.github.com>
2023-11-13 22:10:38 +0000

    improved description

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,9 @@ const About = () => {
     return (
       <><div className="centered-text">
         <p>Somos una pequeña empresa de inmobilarias,
-          la cúal se dedica a enviar los productos a través de internert</p>
+          la cúal se dedica a enviar los productos a través de internert a nivel local y nacional.
+          Nuestra red logística abarca todo el país, lo que nos permite manejar los envíos de manera eficiente. 
+          Aunque seamos una empresa pequeña, nos comprometemos a dar un buen servicio tanto a clientes como a vendedores.</p>
       </div><div className="image-container">
           <img src={PurchasesImg} alt="Purchases" />
         </div></>
